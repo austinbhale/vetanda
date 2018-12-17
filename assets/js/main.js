8b57229490640c21f1ccd74e239b8092d28dcc0a
austinbhale <haleau@live.unc.edu>
2018-12-16 22:32:20 -0500

    Initial contact form styling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,30 @@ window.onscroll = function () {
         $("#nav").addClass("black");
     }
 };
+
+var elementsValid = document.getElementsByClassName('valid');
+var elementsInvalid = document.getElementsByClassName('invalid');
+var valids = false; var invalids = false; var received = false;
+$('#gform').on('submit', function(e) {
+     
+    (elementsValid.length > 2) ? valids = true : valids = false;
+    (elementsInvalid.length == 0) ? invalids = true : invalids = false;
+    
+    if (valids && invalids) {
+        $('#gform *').fadeOut(2000, function() {
+            if (!received) {
+                $('#gform').prepend('Thanks! We\'ll be in touch with you soon.');
+                received = true;
+            }
+        });
+    } else {
+        alert("Please fill in all fields.");
+    }
+});
+
+$( window ).load(function() {
+    setTimeout(function() {
+        $('#parallax-container').addClass('trans');
+    }, 100);
+});
+
