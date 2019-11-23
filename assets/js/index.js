bfa88d61979dc14ca47bba8be1fd2469a72fd6de
austinbhale <haleau@live.unc.edu>
2019-11-23 14:56:52 -0500

    Fix broken link and footer overlay.

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ window.onscroll = function () {
       // $("#nav").addClass("grey darken-3");
       $("#ref").html("contact").attr("href", "contact.htm");
     } else if ($("#four").length && $(window).scrollTop() > $("#four").offset().top - topOffset) {
-      $("#ref").html("events").attr("href", "performances.htm");
+      $("#ref").html("events").attr("href", "events.htm");
     } else if ($("#three").length && $(window).scrollTop() > $("#three").offset().top - topOffset) {
       $("#ref").html("media").attr("href", "media.htm");
     } else if ($("#two").length && $(window).scrollTop() > $("#two").offset().top - topOffset) {
